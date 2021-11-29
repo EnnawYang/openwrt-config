c6e23e43347986fc6fffffbe1599fcdf2cf4e9ca
EnnawYang <EnnawYang@users.noreply.github.com>
2021-11-29 23:31:20 +0800

    Initial commit

---
+++ Newifi-D2-k4-customize.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+
+rm -rf files
+rm -rf package/diy
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 修改机器名称
+sed -i 's/ImmortalWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
+
+# atmaterial主题
+# git clone https://github.com/yangsongli/luci-theme-atmaterial package/mine/luci-theme-atmaterial
+
+# luci-app-ssr-plus
+# git clone https://github.com/fw876/helloworld package/diy/luci-app-ssr-plus
+
+# Modify zzz-default-settings
+sed -i '/exit/d' package/emortal/default-settings/files/zzz-default-settings
+echo "sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release" >> package/emortal/default-settings/files/zzz-default-settings
+echo -e "echo "DISTRIB_REVISION="18.06 build by Yang ($(date +"%Y-%m-%d"))"" >> /etc/openwrt_release" >> package/emortal/default-settings/files/zzz-default-settings
+echo "exit 0" >> package/emortal/default-settings/files/zzz-default-settings
+
+# 更改默认主题
+#sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# 防火墙wan口入站数据修改成ACCEPT(第21行)。如您的网络是公网，建议注释掉此项。
+#sed -i '21s/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
+
+# 允许远程主机连接到本地 SSH 转发端口。如您的网络是公网，建议注释掉此项。
+#sed -i "4a\ \toption GatewayPorts 'on'" package/network/services/dropbear/files/dropbear.config
