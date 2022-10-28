59bf0b842e31fe8809c4f4154918ce7676955c09
EnnawYang <EnnawYang@users.noreply.github.com>
2022-10-28 13:21:04 +0800

    test

--- RM2100-customize.sh
+++ RM2100-customize.sh
@@ -2,16 +2,9 @@
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-([ -d files ] && rm -rf files) || echo "files not found, skip"
-([ -d package/diy ] && rm -rf package/diy) || echo "diy not found, skip"
+rm -rf files
+rm -rf package/diy
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
@@ -23,12 +16,12 @@ sed -i 's/OpenWrt/RM2100/g' package/base-files/files/bin/config_generate
 # git clone https://github.com/yangsongli/luci-theme-atmaterial package/mine/luci-theme-atmaterial
 
 # luci-app-ssr-plus
-git clone https://github.com/fw876/helloworld package/diy/luci-app-ssr-plus
+# git clone https://github.com/fw876/helloworld package/diy/luci-app-ssr-plus
 
 # Modify zzz-default-settings
-export orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-export date_version=$(date +"%Y-%m-%d")
-sed -i "s/${orig_version}/${orig_version} (${date_version})/g" package/lean/default-settings/files/zzz-default-settings
+# export orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# export date_version=$(date +"%Y-%m-%d")
+# sed -i "s/${orig_version}/${orig_version} (${date_version})/g" package/lean/default-settings/files/zzz-default-settings
 
 # 更改默认主题
 #sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
--- x86_64-customize.sh
+++ x86_64-customize.sh
@@ -2,16 +2,9 @@
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-([ -d files ] && rm -rf files) || echo "files not found, skip"
-([ -d package/diy ] && rm -rf package/diy) || echo "diy not found, skip"
+rm -rf files
+rm -rf package/diy
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
@@ -27,19 +20,20 @@ sed -i 's/OpenWrt/x86_64/g' package/base-files/files/bin/config_generate
 # git clone https://github.com/yangsongli/luci-theme-atmaterial package/diy/luci-theme-atmaterial
 
 # luci-theme-argon for 18.06 by jerrykuku
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/diy/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/diy/luci-app-argon-config
+# rm -rf feeds/luci/themes/luci-theme-argon
+# rm -rf package/feeds/luci/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/diy/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-argon-config package/diy/luci-app-argon-config
 
 # luci-app-ssr-plus
-git clone https://github.com/fw876/helloworld package/diy/luci-app-ssr-plus
+# git clone https://github.com/fw876/helloworld package/diy/luci-app-ssr-plus
 
 # v2rayA: A Linux web GUI client of Project V
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/v2raya package/diy/v2raya
-sed -i "s/..\/..\/lang/\$(TOPDIR)\/feeds\/packages\/lang/g" package/diy/v2raya/Makefile
+#svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/v2raya package/diy/v2raya
+#sed -i "s/..\/..\/lang/\$(TOPDIR)\/feeds\/packages\/lang/g" package/diy/v2raya/Makefile
 
 # openwrt-gowebdav
-git clone https://github.com/immortalwrt/openwrt-gowebdav package/diy/webdav
+# git clone https://github.com/immortalwrt/openwrt-gowebdav package/diy/webdav
 
 # A tele-aria2 bot for openwrt
 # git clone https://github.com/EnnawYang/tele-aria2-openwrt package/diy/tele-aria2-openwrt
@@ -48,17 +42,17 @@ git clone https://github.com/immortalwrt/openwrt-gowebdav package/diy/webdav
 # git clone https://github.com/EnnawYang/flowerss-bot-openwrt package/diy/flowerss-bot-openwrt
 
 # luci-app-poweroff by eSir
-git clone https://github.com/esirplayground/luci-app-poweroff package/diy/luci-app-poweroff
+# git clone https://github.com/esirplayground/luci-app-poweroff package/diy/luci-app-poweroff
 
 # Luci-app-dockerman
-git clone https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
+# git clone https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
 # git clone https://github.com/lisaac/luci-lib-docker package/diy/luci-lib-docker
 
 # luci-app-unblockneteasemusic from immortalwrt
-git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic package/diy/luci-app-unblockneteasemusic
+# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/diy/luci-app-unblockneteasemusic
 
 # rtl8812au-ac USB Wi-Fi adapter driver
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac package/diy/rtl8812au-ac
+# svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac package/diy/rtl8812au-ac
 
 # rtl8821cu USB Wi-Fi adapter driver
 # svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8821cu package/diy/rtl8821cu
@@ -67,10 +61,18 @@ svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/pa
 # sed -i "s/START=25/START=99/g" package/lean/luci-app-docker/root/etc/init.d/dockerd
 # sed -i "s/\"\/opt\/\"/\"\/opt\/docker\/\"/g" package/lean/luci-app-docker/root/etc/docker/daemon.json
 
+# update golang 19.x
+# rm -rf feeds/packages/lang/golang
+# rm -rf package/feeds/packages/golang
+# svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+
+# alist ('fatal error: fuse.h: No such file or directory', need install libfuse-dev frist)
+# git clone https://github.com/sbwml/luci-app-alist package/diy/alist
+
 # Modify zzz-default-settings
-export orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-export date_version=$(date +"%Y-%m-%d")
-sed -i "s/${orig_version}/${orig_version} (${date_version})/g" package/lean/default-settings/files/zzz-default-settings
+# export orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# export date_version=$(date +"%Y-%m-%d")
+# sed -i "s/${orig_version}/${orig_version} (${date_version})/g" package/lean/default-settings/files/zzz-default-settings
 
 # zsh
 mkdir -p files/root
@@ -81,6 +83,16 @@ git clone https://github.com/zsh-users/zsh-completions files/root/.oh-my-zsh/cus
 wget https://raw.githubusercontent.com/EnnawYang/openwrt-config/lean-lede/files/.zshrc -O files/root/.zshrc
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
+# Modify distfeeds.conf
+# sed -i '/http/d' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/openwrt_luci/d' package/lean/default-settings/files/zzz-default-settings
+# mkdir -p files/etc/opkg
+# touch files/etc/opkg/distfeeds.conf
+# echo "src/gz openwrt_core https://openwrt.cc/snapshots/targets/x86/64/packages" >> files/etc/opkg/distfeeds.conf
+# echo "src/gz openwrt_base https://openwrt.cc/snapshots/packages/x86_64/base" >> files/etc/opkg/distfeeds.conf
+# echo "src/gz openwrt_luci https://openwrt.cc/snapshots/packages/x86_64/luci" >> files/etc/opkg/distfeeds.conf
+# echo "src/gz openwrt_packages https://openwrt.cc/snapshots/packages/x86_64/packages" >> files/etc/opkg/distfeeds.conf
+
 # 更改默认主题
 #sed -i 's/config internal themes/config internal themes\n    option Rosy  \"\/luci-static\/rosy\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
@@ -89,3 +101,5 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # 允许远程主机连接到本地 SSH 转发端口。如您的网络是公网，建议注释掉此项。
 #sed -i "4a\ \toption GatewayPorts 'on'" package/network/services/dropbear/files/dropbear.config
+
+# ./scripts/feeds install -a -f
